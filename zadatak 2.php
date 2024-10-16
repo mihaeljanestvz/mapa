563981ce7a0fa78d602cefb959c672193d571693
mihaeljanestvz <mihael.janes3@gmail.com>
2024-10-16 21:54:25 +0200

    novi

---
+++ zadatak 2.php
@@ -0,0 +1,23 @@
+<?php
+    $title = "Da Vincijev kod";
+    $link = "https://hr.wikipedia.org/Da_Vincijev_kod";
+?>
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Opis knjige <?php echo $title; ?> autora Dana Browna">
+    <meta name="keywords" content="<?php echo $title; ?>, knjiga, kriminalistički triler, Dan Brown">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+    <p><?php echo $title; ?> je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="<?php echo $link; ?>" target="_blank">Pročitajte više na Wikipediji</a>
+    </p>
+</body>
+</html>
+
+<!-- Naziv dokumenta: zadatak 2.php -->
